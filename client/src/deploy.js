9c27ad804d0287ac683c3b60cbfeb5bf6fc5fb6a
Casey <ujjwalsri81@gmail.com>
2023-05-15 11:55:26 +0530

    client_folder

---
+++ client/src/deploy.js
@@ -0,0 +1,6 @@
+
+    module.exports = { 
+      admin: "0x51e93DE0da219DF7A404B848DfC56a7F98412769", 
+      mktAddress: "0xbD2982Ce52b708f1f740C2361a2f628b38115633", 
+      nftAddress: "0x7C2ca18425B4A221694C747d6E31eFeE7D36af8b"
+    }
